47f4c7b84b5356da6acabb17441ebaa46025e7ff
Nathalie <maskwebs@gmail.com>
2016-07-01 00:35:08 +0200

    Nueva template de página para probar múltiples imagenes destacadas

---
+++ wp-content/themes/sg-window/page-templates/test-page-template.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Template Name: Test Page Template
+ *
+ * @package WordPress
+ * @subpackage sgwindow
+ *
+ */
+
+
+get_header();
+
+?>
+<div class="main-wrapper">
+
+	<div class="site-content"> 
+			<?php
+				if ( have_posts() ) : ?>
+				
+					<div class="content"> 
+
+				<?php
+					while ( have_posts() ) : the_post();
+
+						get_template_part( 'content', 'page' );
+						
+						if ( comments_open() || get_comments_number() ) {
+							comments_template();
+						}
+						
+					endwhile; ?>
+					
+					</div><!-- .content -->
+					<div class="clear"></div>
+				
+				<?php
+
+					sgwindow_paging_nav();
+					
+				else :  
+				?>
+					<div class="content"> 
+					<?php 
+						get_template_part( 'content', 'none' );
+					?>
+					
+					</div><!-- .content -->
+				<?php 
+				endif;
+?>
+	</div><!-- .site-content -->
+	<?php
+	sgwindow_get_sidebar( sgwindow_get_theme_mod('layout_page') );
+	?>
+</div> <!-- .main-wrapper -->
+
+<?php
+get_footer();
